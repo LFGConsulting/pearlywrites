72e98aa94c86ca370983876786be2b3f304ff36b
grizzdank <dave.e.graham@gmail.com>
2025-04-16 19:07:45 -0700

    fix: update types for dynamic route params

--- src/app/(routes)/blog/[slug]/page.tsx
+++ src/app/(routes)/blog/[slug]/page.tsx
@@ -3,13 +3,12 @@ import { Metadata } from 'next'
 import { getPostBySlug } from '@/lib/ghost/utils'
 import { formatDate } from '@/lib/utils'
 
-type Props = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
 // Generate metadata for SEO
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
   const post = await getPostBySlug(params.slug)
   
   if (!post) {
@@ -33,7 +32,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export const revalidate = 3600 // Revalidate every hour
 
-export default async function BlogPost({ params, searchParams }: Props) {
+export default async function BlogPost({
+  params,
+}: {
+  params: { slug: string }
+}) {
   const post = await getPostBySlug(params.slug)
 
   if (!post) {
